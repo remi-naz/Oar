48718e4abc382c5bbc5464f2de449574f5658c37
Ridill <69680701+remi-naz@users.noreply.github.com>
2026-07-25 11:43:07 +0300

    feat(money-pile): implement Add/Edit Money Pile functionality (#166)

--- app/src/main/java/dev/ridill/oar/moneyPiles/presentation/nav/MoneyPileNavEntries.kt
+++ app/src/main/java/dev/ridill/oar/moneyPiles/presentation/nav/MoneyPileNavEntries.kt
@@ -1,16 +1,24 @@
 package dev.ridill.oar.moneyPiles.presentation.nav
 
 import androidx.compose.material3.MotionScheme
+import androidx.compose.runtime.getValue
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import androidx.paging.compose.collectAsLazyPagingItems
+import dev.ridill.oar.core.ui.components.CollectFlowEffect
 import dev.ridill.oar.core.ui.components.rememberSnackbarController
+import dev.ridill.oar.core.ui.navigation.AddEditMoneyPileResult
 import dev.ridill.oar.core.ui.navigation.AddEditMoneyPileRoute
 import dev.ridill.oar.core.ui.navigation.AllMoneyPilesRoute
+import dev.ridill.oar.core.ui.navigation.LocalResultBus
 import dev.ridill.oar.core.ui.navigation.MoneyPileDetailsRoute
 import dev.ridill.oar.core.ui.navigation.MoneyPileFundMovementRoute
 import dev.ridill.oar.core.ui.navigation.OarNavigator
+import dev.ridill.oar.core.ui.navigation.ResultEffect
+import dev.ridill.oar.moneyPiles.presentation.addEditPile.AddEditPileScreen
+import dev.ridill.oar.moneyPiles.presentation.addEditPile.AddEditPileViewModel
 import dev.ridill.oar.moneyPiles.presentation.allPiles.AllPilesScreen
 import dev.ridill.oar.moneyPiles.presentation.allPiles.AllPilesViewModel
 
@@ -23,16 +31,68 @@ fun EntryProviderScope<NavKey>.moneyPileEntries(
         val pilesPagingItems = viewModel.pilesPagingData.collectAsLazyPagingItems()
         val snackbarController = rememberSnackbarController()
 
+        ResultEffect<AddEditMoneyPileResult> { result ->
+            viewModel.onAddEditPileResult(result)
+        }
+
+        CollectFlowEffect(viewModel.events) { event ->
+            when (event) {
+                is AllPilesViewModel.AllPilesEvent.ShowUiMessage -> {
+                    snackbarController.showSnackbar(event.text)
+                }
+            }
+        }
+
         AllPilesScreen(
             snackbarController = snackbarController,
             pilesPagingItems = pilesPagingItems,
-            navigateToAddPile = {},
-            navigateToPileDetails = {},
+            navigateToAddPile = { navigator.navigate(AddEditMoneyPileRoute()) },
+            navigateToPileDetails = { navigator.navigate(AddEditMoneyPileRoute(it)) },
             navigateToAddToPile = {},
             navigateUp = navigator::goBack,
         )
     }
+
     entry<MoneyPileDetailsRoute> {}
-    entry<AddEditMoneyPileRoute> {}
+    entry<AddEditMoneyPileRoute> { route ->
+        val viewModel: AddEditPileViewModel =
+            hiltViewModel<AddEditPileViewModel, AddEditPileViewModel.Factory>(
+                creationCallback = { it.create(route) }
+            )
+        val resultBus = LocalResultBus.current
+        val state by viewModel.state.collectAsStateWithLifecycle()
+
+        val snackbarController = rememberSnackbarController()
+        CollectFlowEffect(viewModel.events) { event ->
+            when (event) {
+                AddEditPileViewModel.AddEditPileEvent.PileSaved -> {
+                    resultBus.sendResult(AddEditMoneyPileResult.PILE_SAVED)
+                    navigator.goBack()
+                }
+
+                is AddEditPileViewModel.AddEditPileEvent.ShowUiMessage -> {
+                    snackbarController.showSnackbar(event.text)
+                }
+
+                AddEditPileViewModel.AddEditPileEvent.PileDeleted -> {
+                    resultBus.sendResult(AddEditMoneyPileResult.PILE_DELETED)
+                    navigator.goBack()
+                }
+            }
+        }
+
+        AddEditPileScreen(
+            state = state,
+            isEditMode = viewModel.isEditMode,
+            nameState = viewModel.nameState,
+            starterAmountState = viewModel.starterAmountState,
+            targetAmountState = viewModel.targetAmountState,
+            reminderAmountState = viewModel.reminderAmountState,
+            actions = viewModel,
+            navigateUp = navigator::goBack,
+            snackbarController = snackbarController,
+        )
+    }
+
     entry<MoneyPileFundMovementRoute> {}
 }
