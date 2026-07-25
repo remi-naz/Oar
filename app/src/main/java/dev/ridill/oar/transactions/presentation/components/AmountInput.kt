5adbb177e44f60966f2e4a8097403673d05f9049
Ridill <69680701+remi-naz@users.noreply.github.com>
2026-07-25 11:43:07 +0300

    feat(money-pile): implement Add/Edit Money Pile functionality (#166)

--- app/src/main/java/dev/ridill/oar/transactions/presentation/components/AmountInput.kt
+++ app/src/main/java/dev/ridill/oar/transactions/presentation/components/AmountInput.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
@@ -26,6 +27,7 @@ import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -37,7 +39,6 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import dev.ridill.oar.R
 import dev.ridill.oar.budgetCycles.presentation.currencyUpdate.CurrencySelectionSheet
 import dev.ridill.oar.budgetCycles.presentation.currencyUpdate.CurrencySelectionViewModel
-import dev.ridill.oar.core.domain.util.LocaleUtil
 import dev.ridill.oar.core.domain.util.Zero
 import dev.ridill.oar.core.domain.util.orZero
 import dev.ridill.oar.core.ui.components.ComponentViewModelScope
@@ -52,9 +53,13 @@ import java.util.Currency
 @Composable
 fun AmountInput(
     inputState: TextFieldState,
+    currency: Currency,
+    onCurrencySelect: (Currency) -> Unit,
     modifier: Modifier = Modifier,
-    currency: Currency = LocaleUtil.defaultCurrency,
-    onCurrencySelect: (Currency) -> Unit = {},
+    label: String? = null,
+    placeholder: String? = stringResource(R.string.amount_zero),
+    colors: TextFieldColors = DefaultColors,
+    prefix: @Composable (() -> Unit)? = { Text(currency.symbol) },
     isInputAnExpression: Boolean = false,
     onExpressionEvalClick: () -> Unit = {},
     focusManager: FocusManager = LocalFocusManager.current,
@@ -62,6 +67,7 @@ fun AmountInput(
     onKeyboardAction: KeyboardActionHandler? = { focusManager.moveFocus(FocusDirection.Next) },
     lineLimits: TextFieldLineLimits = TextFieldLineLimits.SingleLine,
 ) {
+    if (LocalInspectionMode.current) return
     val showTransformButton by remember {
         derivedStateOf {
             inputState.text.toString()
@@ -89,24 +95,17 @@ fun AmountInput(
             textStyle = MaterialTheme.typography.headlineMedium.copy(
                 textAlign = TextAlign.Center
             ),
-            placeholder = {
-                Text(
-                    text = stringResource(R.string.amount_zero),
-                    style = MaterialTheme.typography.headlineMedium,
-                    modifier = Modifier
-                        .defaultMinSize(minWidth = InputMinWidth),
-                    textAlign = TextAlign.Center
-                )
+            label = label?.let {
+                { Text(it) }
             },
+            placeholder = placeholder?.let {
+                { Text(it) }
+            },
+            prefix = prefix,
             keyboardOptions = keyboardOptions.copy(
                 keyboardType = KeyboardType.Phone,
             ),
-            colors = TextFieldDefaults.colors(
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                focusedContainerColor = Color.Transparent,
-                unfocusedContainerColor = Color.Transparent,
-            ),
+            colors = colors,
             onKeyboardAction = onKeyboardAction,
             lineLimits = lineLimits,
             outputTransformation = rememberAmountOutputTransformation(),
@@ -177,4 +176,108 @@ fun AmountInput(
     }
 }
 
+@Composable
+fun AmountInput(
+    inputState: TextFieldState,
+    currency: Currency,
+    modifier: Modifier = Modifier,
+    label: String? = null,
+    placeholder: String? = stringResource(R.string.amount_zero),
+    colors: TextFieldColors = DefaultColors,
+    prefix: @Composable (() -> Unit)? = { Text(currency.symbol) },
+    isInputAnExpression: Boolean = false,
+    onExpressionEvalClick: () -> Unit = {},
+    focusManager: FocusManager = LocalFocusManager.current,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    onKeyboardAction: KeyboardActionHandler? = { focusManager.moveFocus(FocusDirection.Next) },
+    lineLimits: TextFieldLineLimits = TextFieldLineLimits.SingleLine,
+) {
+    if (LocalInspectionMode.current) return
+    val showTransformButton by remember {
+        derivedStateOf {
+            inputState.text.toString()
+                .toDoubleOrNull().orZero() > Double.Zero
+        }
+    }
+    var showTransformationSheet by rememberSaveable { mutableStateOf(false) }
+
+    ComponentViewModelScope("amount_input") {
+        val amountTransformationViewModel: AmountTransformationViewModel = hiltViewModel()
+
+        OarTextField(
+            state = inputState,
+            modifier = modifier
+                .defaultMinSize(minWidth = InputMinWidth),
+            textStyle = MaterialTheme.typography.headlineMedium.copy(
+                textAlign = TextAlign.Center
+            ),
+            label = label?.let {
+                { Text(it) }
+            },
+            placeholder = placeholder?.let {
+                { Text(it) }
+            },
+            prefix = prefix,
+            keyboardOptions = keyboardOptions.copy(
+                keyboardType = KeyboardType.Phone,
+            ),
+            colors = colors,
+            onKeyboardAction = onKeyboardAction,
+            lineLimits = lineLimits,
+            outputTransformation = rememberAmountOutputTransformation(),
+            trailingIcon = {
+                when {
+                    isInputAnExpression -> {
+                        IconButton(onClick = onExpressionEvalClick) {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.ic_rounded_equals),
+                                contentDescription = stringResource(R.string.cd_evaluate_expression),
+                                modifier = Modifier
+                                    .size(IconSizeMedium)
+                            )
+                        }
+                    }
+
+                    showTransformButton -> {
+                        IconButton(onClick = { showTransformationSheet = true }) {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.ic_rounded_gears),
+                                contentDescription = stringResource(R.string.cd_transform_amount)
+                            )
+                        }
+                    }
+                }
+            }
+        )
+
+        if (showTransformationSheet) {
+            val selectedTransformation by amountTransformationViewModel.selectedTransformation.collectAsStateWithLifecycle()
+
+            OarModalBottomSheet(
+                onDismissRequest = { showTransformationSheet = false }
+            ) {
+                AmountTransformationSheet(
+                    selectedTransformation = selectedTransformation,
+                    onTransformationSelect = amountTransformationViewModel::onTransformationSelect,
+                    factorInput = amountTransformationViewModel.factorInputState,
+                    onTransformClick = {
+                        inputState.setTextAndPlaceCursorAtEnd(
+                            amountTransformationViewModel
+                                .transformedAmount(inputState.text.toString())
+                        )
+                        showTransformationSheet = false
+                    }
+                )
+            }
+        }
+    }
+}
+
 private val InputMinWidth = 160.dp
+private val DefaultColors: TextFieldColors
+    @Composable get() = TextFieldDefaults.colors(
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        focusedContainerColor = Color.Transparent,
+        unfocusedContainerColor = Color.Transparent,
+    )
