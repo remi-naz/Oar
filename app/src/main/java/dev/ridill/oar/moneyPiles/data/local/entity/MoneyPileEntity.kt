683269f3ef312cb5a52a15ae1e31d69b4d724009
Ridill <69680701+remi-naz@users.noreply.github.com>
2026-07-25 10:56:13 +0300

    feat(money-pile): initialize data layer and resources for Money Piles (#162)

---
+++ app/src/main/java/dev/ridill/oar/moneyPiles/data/local/entity/MoneyPileEntity.kt
@@ -0,0 +1,56 @@
+package dev.ridill.oar.moneyPiles.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import dev.ridill.oar.core.data.db.OarDatabase
+import dev.ridill.oar.moneyPiles.domain.model.PileContributionMode
+import dev.ridill.oar.moneyPiles.domain.model.PileIcon
+import dev.ridill.oar.moneyPiles.domain.model.PileReminderBehavior
+import dev.ridill.oar.moneyPiles.domain.model.PileReminderCadence
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "money_pile_table",
+    indices = [Index("name")]
+)
+data class MoneyPileEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long = OarDatabase.DEFAULT_ID_LONG,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "icon")
+    val icon: PileIcon,
+
+    @ColumnInfo(name = "color")
+    val color: Int,
+
+    @ColumnInfo(name = "contribution_mode")
+    val contributionMode: PileContributionMode,
+
+    @ColumnInfo(name = "reminder_cadence")
+    val reminderCadence: PileReminderCadence,
+
+    @ColumnInfo(name = "reminder_behavior")
+    val reminderBehavior: PileReminderBehavior,
+
+    @ColumnInfo(name = "locked")
+    val locked: Boolean,
+
+    @ColumnInfo(name = "currency_code")
+    val currencyCode: String,
+
+    @ColumnInfo(name = "target_amount")
+    val targetAmount: Double?,
+
+    @ColumnInfo(name = "target_date")
+    val targetDate: LocalDate?,
+
+    @ColumnInfo(name = "created_timestamp")
+    val createdTimestamp: LocalDateTime,
+)
