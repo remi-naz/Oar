90f68fea223f8abd6e1aff59c20bf24b69c2a47e
RemijiusBrian <remijius.b@applab.qa>
2026-08-06 10:56:42 +0300

    feat(money-pile): update pile transaction list ordering to desc by timestamp

--- app/src/main/java/dev/ridill/oar/moneyPiles/data/local/PileTransactionKey.kt
+++ app/src/main/java/dev/ridill/oar/moneyPiles/data/local/PileTransactionKey.kt
@@ -1,12 +1,17 @@
 package dev.ridill.oar.moneyPiles.data.local
 
 import dev.ridill.oar.core.data.db.KeysetPageKey
+import dev.ridill.oar.core.data.db.toSqliteUtcDateTimeString
 import dev.ridill.oar.moneyPiles.data.local.entity.MoneyPileTransactionsEntity
 
 class PileTransactionKey(
+    val createdTimestamp: String,
     val id: Long,
 ) : KeysetPageKey {
-    override fun toValues(): List<Any> = listOf(id)
+    override fun toValues(): List<Any> = listOf(createdTimestamp, id)
 }
 
-fun MoneyPileTransactionsEntity.toKey() = PileTransactionKey(id)
+fun MoneyPileTransactionsEntity.toKey() = PileTransactionKey(
+    createdTimestamp = createdTimestamp.toSqliteUtcDateTimeString(),
+    id = id
+)
