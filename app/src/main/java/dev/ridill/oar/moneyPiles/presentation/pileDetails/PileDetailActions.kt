c82ae8b5a2b56f415ff5d6c77ea52b327c3c7d31
RemijiusBrian <remijius.b@applab.qa>
2026-08-06 11:33:37 +0300

    feat(money-pile): add show/hide locked entries toggle

--- app/src/main/java/dev/ridill/oar/moneyPiles/presentation/pileDetails/PileDetailActions.kt
+++ app/src/main/java/dev/ridill/oar/moneyPiles/presentation/pileDetails/PileDetailActions.kt
@@ -3,4 +3,5 @@ package dev.ridill.oar.moneyPiles.presentation.pileDetails
 interface PileDetailActions {
     fun onTransactionActionRevealed()
     fun onTransactionDelete(id: Long)
+    fun onIncludeLockedTransactionsToggle(includeLocked: Boolean)
 }
